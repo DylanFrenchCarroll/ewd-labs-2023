fa4fc19296753d080cda75e43b0f68fb55335ae5
Dylan French Carroll <Dylan.French.Carroll@gmail.com>
2023-05-13 12:29:13 +0000

    Lab 01 - Work

---
+++ index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './src/movies/index.js';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+app.use(express.json());
+
+
+app.use('/api/movies', moviesRouter);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
