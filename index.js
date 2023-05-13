d539babccb3aac74018745f302ff12565734772e
Dylan French Carroll <Dylan.French.Carroll@gmail.com>
2023-05-13 14:02:27 +0000

    Movies endpoints

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './src/movies/index.js';
-import genreRouter from './src/genres/index.js';
 import createAccountsRouter from './src/accounts/routes/index.js';
 import buildDependencies from "./src/config/dependencies.js";
+import createMoviesRouter from './src/movies/routes/index.js';
 
 dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT;
-app.use(express.json());
 const dependencies = buildDependencies();
 
+app.use(express.json());
+
 app.use('/api/accounts', createAccountsRouter(dependencies));
+app.use('/api/movies', createMoviesRouter(dependencies));
 
-app.use('/api/movies', moviesRouter);
 
-app.use('/api/genres', genreRouter);
+
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
