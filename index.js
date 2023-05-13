02e98ecc0a2bd83944101a9590c50fd19c4c01eb
Dylan French Carroll <Dylan.French.Carroll@gmail.com>
2023-05-13 13:56:31 +0000

    Accounts

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './src/movies/index.js';
 import genreRouter from './src/genres/index.js';
+import createAccountsRouter from './src/accounts/routes/index.js';
+import buildDependencies from "./src/config/dependencies.js";
+
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
 app.use(express.json());
+const dependencies = buildDependencies();
 
+app.use('/api/accounts', createAccountsRouter(dependencies));
 
 app.use('/api/movies', moviesRouter);
 
